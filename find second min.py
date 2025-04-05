d02f382be851baf6f54161f0b08342aa131a6857
prwnvs <kaktys382@gmail.com>
2025-04-05 18:45:22 +0300

    Update find second min.py

--- find second min.py
+++ find second min.py
@@ -1,1 +1,21 @@
+def find_min2(array):
+  if len(array) < 2:
+    return None
+
+  min1 = float('inf')
+  min2 = float('inf')
+
+  for num in array:
+    if num < min1:
+      min2 = min1
+      min1 = num
+    elif num < min2 and num != min1:
+      min2 = num
+
+  return min2 if min2 != float('inf') else None
+
+array = [8, 6, 4, 2, 5, 9, 4, 6, 5]
+second_min = find_min2(array)
+print(second_min)
+
 
